e96aa30d2cb6cbb3a6ad9a41c81ffa2719c9a712
Rohit Mohan <rohit.mohan@gmail.com>
2020-01-30 18:24:58 -0500

    added flask_request_params for easier handling of request params

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -2,8 +2,12 @@ end = 0
 
 from .view import View
 
+from flask_request_params import bind_request_params
+
 from .users_view import UsersView
 
 def initialize(app):
+    app.before_request(bind_request_params)
+
     UsersView.register(app, base_class=View)
 end
